dc84adb253dc305e1a37bbd876b61d9eeb7ae22c
Cyrus Diego <cyrus.d04@gmail.com>
2020-09-05 09:47:32 -0600

    cleaned up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import SimpleMap from './components/Map';
+import { SimpleMap } from './components/Map';
 import './App.css';
 
-function App() {
+export const App = () => {
+  const mapProps = {
+    // Edmonton
+    center: {
+      lat: 53.54,
+      lng: -113.49,
+    },
+    zoom: 11,
+  };
+
   return (
     <div className='App' width='50' height='50'>
-      <SimpleMap />
+      <SimpleMap center={mapProps.center} zoom={mapProps.zoom} />
     </div>
   );
-}
-
-export default App;
+};
--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -1,31 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33,
-    },
-    zoom: 11,
-  };
-
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: /* INSERT GOOGLE API KEY HERE */ }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent lat={59.955413} lng={30.337844} text='My Marker' />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-}
-
-export default SimpleMap;
+export const SimpleMap = (props) => {
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyCmSYG-wJ82zCvH4nOdsa4v1q8lNWTQ0JA' }}
+        defaultCenter={props.center}
+        defaultZoom={props.zoom}
+      />
+    </div>
+  );
+};
