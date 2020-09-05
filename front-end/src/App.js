639ac795f905db1662f49bd392ef57535a24310c
Cyrus Diego <cyrus.d04@gmail.com>
2020-09-04 23:27:50 -0600

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import SimpleMap from './components/Map';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App' width='50' height='50'>
+      <SimpleMap />
+    </div>
+  );
+}
+
+export default App;
