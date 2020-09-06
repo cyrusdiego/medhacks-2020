fd5264a03cef5a580f715c54e96daa4a59c78813
Cyrus Diego <cyrus.d04@gmail.com>
2020-09-05 21:25:31 -0600

    Multiple heatmap layers can be added

--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -8,14 +8,24 @@ const myLocation = {
   lat: 53.54,
   lng: -113.49,
 };
+
+const RADIUS_SCALE = 1000;
+const WEIGHT_SCALE = 1000;
+
 // styles
 const mapStyles = {
   width: '100%',
   height: '900px',
 };
 
-const testServer = () => {
+const getTable = () => {
   return axios('http://localhost:3001/getTable', {
+    method: 'GET',
+  });
+};
+
+const getRiskFactors = () => {
+  return axios('http://localhost:3001/getRiskFactors', {
     method: 'GET',
   });
 };
@@ -24,8 +34,6 @@ function GoogleMaps(props) {
   // refs
   const googleMapRef = React.createRef();
   const googleMap = useRef(null);
-  const marker = useRef(null);
-  const heatMap = useRef(null);
 
   // helper functions
   const createGoogleMap = () =>
@@ -35,131 +43,142 @@ function GoogleMaps(props) {
         lat: myLocation.lat,
         lng: myLocation.lng,
       },
-      // styles: [
-      //   { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-      //   { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-      //   { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-      //   {
-      //     featureType: 'administrative.locality',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{ color: '#d59563' }],
-      //   },
-      //   {
-      //     featureType: 'poi',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{ color: '#d59563' }],
-      //   },
-      //   {
-      //     featureType: 'poi.park',
-      //     elementType: 'geometry',
-      //     stylers: [{ color: '#263c3f' }],
-      //   },
-      //   {
-      //     featureType: 'poi.park',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{ color: '#6b9a76' }],
-      //   },
-      //   {
-      //     featureType: 'road',
-      //     elementType: 'geometry',
-      //     stylers: [{ color: '#38414e' }],
-      //   },
-      //   {
-      //     featureType: 'road',
-      //     elementType: 'geometry.stroke',
-      //     stylers: [{ color: '#212a37' }],
-      //   },
-      //   {
-      //     featureType: 'road',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{ color: '#9ca5b3' }],
-      //   },
-      //   {
-      //     featureType: 'road.highway',
-      //     elementType: 'geometry',
-      //     stylers: [{ color: '#746855' }],
-      //   },
-      //   {
-      //     featureType: 'road.highway',
-      //     elementType: 'geometry.stroke',
-      //     stylers: [{ color: '#1f2835' }],
-      //   },
-      //   {
-      //     featureType: 'road.highway',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{ color: '#f3d19c' }],
-      //   },
-      //   {
-      //     featureType: 'transit',
-      //     elementType: 'geometry',
-      //     stylers: [{ color: '#2f3948' }],
-      //   },
-      //   {
-      //     featureType: 'transit.station',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{ color: '#d59563' }],
-      //   },
-      //   {
-      //     featureType: 'water',
-      //     elementType: 'geometry',
-      //     stylers: [{ color: '#17263c' }],
-      //   },
-      //   {
-      //     featureType: 'water',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{ color: '#515c6d' }],
-      //   },
-      //   {
-      //     featureType: 'water',
-      //     elementType: 'labels.text.stroke',
-      //     stylers: [{ color: '#17263c' }],
-      //   },
-      // ],
+      styles: [
+        { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+        { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+        { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{ color: '#d59563' }],
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{ color: '#d59563' }],
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{ color: '#263c3f' }],
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{ color: '#6b9a76' }],
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{ color: '#38414e' }],
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{ color: '#212a37' }],
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{ color: '#9ca5b3' }],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{ color: '#746855' }],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{ color: '#1f2835' }],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{ color: '#f3d19c' }],
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{ color: '#2f3948' }],
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{ color: '#d59563' }],
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{ color: '#17263c' }],
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{ color: '#515c6d' }],
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{ color: '#17263c' }],
+        },
+      ],
     });
 
-  const createMarker = () =>
+  const createMarker = (lat, lng) =>
     new window.google.maps.Marker({
-      position: { lat: myLocation.lat, lng: myLocation.lng },
+      position: { lat: lat, lng: lng },
       map: googleMap.current,
     });
 
-  const getPoints = (building) => {
-    const coords = building.rows[0].center;
-    return [
-      {
-        location: new window.google.maps.LatLng(
-          coords.latitude,
-          coords.longitude
-        ),
-        weight: 10,
-        radius: 10,
-        opacity: 1,
-      },
-    ];
+  const getPoint = (building, riskFactor) => {
+    let dataPoints = [];
+    // console.log('size: ' + rbuilding.size);
+    // console.log('weight: ' + riskFactors[i] * rows[i].size * WEIGHT_SCALE);
+    // console.log('radius: ' + riskFactors[i] * rows[i].size * RADIUS_SCALE);
+    dataPoints.push({
+      location: new window.google.maps.LatLng(
+        building.center.latitude,
+        building.center.longitude
+      ),
+      weight: riskFactor * building.size * WEIGHT_SCALE,
+      opacity: 1,
+    });
+    return dataPoints;
   };
-  const createHeatMap = (building, googleMap) => {
-    const points = getPoints(building);
-    return new window.google.maps.visualization.HeatmapLayer({
-      data: points,
+
+  const createHeatMap = (building, googleMap, riskFactors, i) => {
+    const point = getPoint(building, riskFactors);
+    new window.google.maps.visualization.HeatmapLayer({
+      data: point,
       map: googleMap,
+      options: {
+        radius: 10,
+      },
     });
   };
 
   // useEffect Hook
   useEffect(() => {
-    let building;
-    testServer().then((resp) => {
-      building = resp.data;
-      const googleMapScript = document.createElement('script');
-      googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=visualization`;
-      window.document.body.appendChild(googleMapScript);
+    let buildings;
+    let riskFactors;
+    getTable().then((resp) => {
+      buildings = resp.data;
+      getRiskFactors().then((resp) => {
+        riskFactors = resp.data;
+        const googleMapScript = document.createElement('script');
+        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=visualization`;
+        window.document.body.appendChild(googleMapScript);
 
-      googleMapScript.addEventListener('load', () => {
-        googleMap.current = createGoogleMap();
-        heatMap.current = createHeatMap(building, googleMap.current);
+        googleMapScript.addEventListener('load', () => {
+          googleMap.current = createGoogleMap();
+          for (let i = 0; i < buildings.rows.length; i++) {
+            createHeatMap(buildings.rows[i], googleMap.current, riskFactors[i]);
+          }
+        });
       });
     });
-  });
+  }, []);
 
   return <div id='google-map' ref={googleMapRef} style={mapStyles} />;
 }
