c4a87058cb92526e9a5287ae504201135999e813
Cyrus Diego <cyrus.d04@gmail.com>
2020-09-05 19:56:49 -0600

    Added initial heatmap

--- front-end/src/components/Map.jsx
+++ front-end/src/components/Map.jsx
@@ -25,6 +25,7 @@ function GoogleMaps(props) {
   const googleMapRef = React.createRef();
   const googleMap = useRef(null);
   const marker = useRef(null);
+  const heatMap = useRef(null);
 
   // helper functions
   const createGoogleMap = () =>
@@ -34,86 +35,86 @@ function GoogleMaps(props) {
         lat: myLocation.lat,
         lng: myLocation.lng,
       },
-      styles: [
-        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-        {
-          featureType: "administrative.locality",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }]
-        },
-        {
-          featureType: "poi",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }]
-        },
-        {
-          featureType: "poi.park",
-          elementType: "geometry",
-          stylers: [{ color: "#263c3f" }]
-        },
-        {
-          featureType: "poi.park",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#6b9a76" }]
-        },
-        {
-          featureType: "road",
-          elementType: "geometry",
-          stylers: [{ color: "#38414e" }]
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#212a37" }]
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#9ca5b3" }]
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry",
-          stylers: [{ color: "#746855" }]
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#1f2835" }]
-        },
-        {
-          featureType: "road.highway",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#f3d19c" }]
-        },
-        {
-          featureType: "transit",
-          elementType: "geometry",
-          stylers: [{ color: "#2f3948" }]
-        },
-        {
-          featureType: "transit.station",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }]
-        },
-        {
-          featureType: "water",
-          elementType: "geometry",
-          stylers: [{ color: "#17263c" }]
-        },
-        {
-          featureType: "water",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#515c6d" }]
-        },
-        {
-          featureType: "water",
-          elementType: "labels.text.stroke",
-          stylers: [{ color: "#17263c" }]
-        }
-      ]
+      // styles: [
+      //   { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+      //   { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+      //   { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+      //   {
+      //     featureType: 'administrative.locality',
+      //     elementType: 'labels.text.fill',
+      //     stylers: [{ color: '#d59563' }],
+      //   },
+      //   {
+      //     featureType: 'poi',
+      //     elementType: 'labels.text.fill',
+      //     stylers: [{ color: '#d59563' }],
+      //   },
+      //   {
+      //     featureType: 'poi.park',
+      //     elementType: 'geometry',
+      //     stylers: [{ color: '#263c3f' }],
+      //   },
+      //   {
+      //     featureType: 'poi.park',
+      //     elementType: 'labels.text.fill',
+      //     stylers: [{ color: '#6b9a76' }],
+      //   },
+      //   {
+      //     featureType: 'road',
+      //     elementType: 'geometry',
+      //     stylers: [{ color: '#38414e' }],
+      //   },
+      //   {
+      //     featureType: 'road',
+      //     elementType: 'geometry.stroke',
+      //     stylers: [{ color: '#212a37' }],
+      //   },
+      //   {
+      //     featureType: 'road',
+      //     elementType: 'labels.text.fill',
+      //     stylers: [{ color: '#9ca5b3' }],
+      //   },
+      //   {
+      //     featureType: 'road.highway',
+      //     elementType: 'geometry',
+      //     stylers: [{ color: '#746855' }],
+      //   },
+      //   {
+      //     featureType: 'road.highway',
+      //     elementType: 'geometry.stroke',
+      //     stylers: [{ color: '#1f2835' }],
+      //   },
+      //   {
+      //     featureType: 'road.highway',
+      //     elementType: 'labels.text.fill',
+      //     stylers: [{ color: '#f3d19c' }],
+      //   },
+      //   {
+      //     featureType: 'transit',
+      //     elementType: 'geometry',
+      //     stylers: [{ color: '#2f3948' }],
+      //   },
+      //   {
+      //     featureType: 'transit.station',
+      //     elementType: 'labels.text.fill',
+      //     stylers: [{ color: '#d59563' }],
+      //   },
+      //   {
+      //     featureType: 'water',
+      //     elementType: 'geometry',
+      //     stylers: [{ color: '#17263c' }],
+      //   },
+      //   {
+      //     featureType: 'water',
+      //     elementType: 'labels.text.fill',
+      //     stylers: [{ color: '#515c6d' }],
+      //   },
+      //   {
+      //     featureType: 'water',
+      //     elementType: 'labels.text.stroke',
+      //     stylers: [{ color: '#17263c' }],
+      //   },
+      // ],
     });
 
   const createMarker = () =>
@@ -122,18 +123,41 @@ function GoogleMaps(props) {
       map: googleMap.current,
     });
 
+  const getPoints = (building) => {
+    const coords = building.rows[0].center;
+    return [
+      {
+        location: new window.google.maps.LatLng(
+          coords.latitude,
+          coords.longitude
+        ),
+        weight: 10,
+        radius: 10,
+        opacity: 1,
+      },
+    ];
+  };
+  const createHeatMap = (building, googleMap) => {
+    const points = getPoints(building);
+    return new window.google.maps.visualization.HeatmapLayer({
+      data: points,
+      map: googleMap,
+    });
+  };
+
   // useEffect Hook
   useEffect(() => {
+    let building;
     testServer().then((resp) => {
-      console.log(resp);
-    });
-    const googleMapScript = document.createElement('script');
-    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`;
-    window.document.body.appendChild(googleMapScript);
+      building = resp.data;
+      const googleMapScript = document.createElement('script');
+      googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=visualization`;
+      window.document.body.appendChild(googleMapScript);
 
-    googleMapScript.addEventListener('load', () => {
-      googleMap.current = createGoogleMap();
-      // marker.current = createMarker();
+      googleMapScript.addEventListener('load', () => {
+        googleMap.current = createGoogleMap();
+        heatMap.current = createHeatMap(building, googleMap.current);
+      });
     });
   });
 
